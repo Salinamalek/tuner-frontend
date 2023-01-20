2c2368ad111aa22347ea2e326ebaaa94c7b8fc0d
Salinamalek <salinamalek@pursuit.org>
2023-01-20 15:52:26 -0500

    new port

---
+++ src/Components/SongNewForm.js
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const API = process.env.REACT_APP_API_URL;
+
+function SongNewForm() {
+  let navigate = useNavigate();
+
+  const addSong = (newSong) => {
+    axios
+      .post(`${API}/songs`, newSong)
+      .then(
+        () => {
+          navigate(`/songs`);
+        },
+        (error) => console.error(error)
+      )
+      .catch((c) => console.warn("catch", c));
+  };
+
+  const [song, setSong] = useState({
+    name: "",
+    artist: "",
+    album: "",
+    time: "",
+    is_favorite: false,
+  });
+
+  const handleTextChange = (event) => {
+    setSong({ ...song, [event.target.id]: event.target.value });
+  };
+
+  const handleCheckboxChange = () => {
+    setSong({ ...song, is_favorite: !song.is_favorite });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addSong(song);
+  };
+  return (
+    <div className="New">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          value={song.name}
+          type="text"
+          onChange={handleTextChange}
+          placeholder="Name of Song"
+          required
+        />
+        <label htmlFor="artist">Artist:</label>
+        <input
+          id="artist"
+          type="text"
+          //   pattern="http[s]*://.+"
+          //   required
+          value={song.artist}
+          placeholder="Name of Artist"
+          onChange={handleTextChange}
+        />
+        <label htmlFor="album">Album:</label>
+        <input
+          id="album"
+          type="text"
+          name="album"
+          value={song.album}
+          placeholder="Album Title"
+          onChange={handleTextChange}
+        />
+        <label htmlFor="is_favorite">Favorite:</label>
+        <input
+          id="is_favorite"
+          type="checkbox"
+          onChange={handleCheckboxChange}
+          checked={song.is_favorite}
+        />
+
+        <br />
+
+        <input type="submit" />
+      </form>
+      {/* <Link to={`/songs/${id}`}>
+        <button>Nevermind!</button>
+      </Link> */}
+    </div>
+  );
+}
+
+export default SongNewForm;
